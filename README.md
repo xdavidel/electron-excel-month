17c868ab096278bb30378d2d867aa647e1e8c7a3
David Delarosa <xdavidel@gmail.com>
2017-12-11 15:40:20 +0200

    fix table and UI

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Month Excel Creator
 
-This app creates a month representation in excel
+An Electron app that generate Excel document for a selected month and year
+
+## Getting Started
+
+Before any other step - install dependencies using npm
+
+```
+npm install
+```
+
+### Building
+
+To create a binary for windows x64 use:
+
+```
+npm run build
+```
+
+## Development
+
+To run in dev mode use:
+
+```
+npm start
+```
+
--- pages/main-page/main-page.js
+++ pages/main-page/main-page.js
@@ -1,26 +1,36 @@
 const Excel = require('excel4node')
 
 const yearPicker = document.getElementById('year-picker')
+const monthPicker = document.getElementById('month-picker')
 const generateBtn = document.getElementById('generateBtn')
+const travelPicker = document.getElementById('travel-picker')
 
-const TITLES = ['Day in Week', 'Day of Month', 'Hours', 'Extra Hours', 'Travels', 'Notes']
+const COLUMNS = ['Day in Week', 'Day of Month', 'Hours', 'Extra Hours', 'Travels', 'Notes']
 const DAYS_OF_WEEK = { 0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday" }
 const FULL_HOURS = 9
 
 
 let currentDate = new Date()
+let travelBase = 5.9
+travelPicker.value = travelBase
+
+travelPicker.addEventListener('input', ev => {
+    if (travelPicker.value != '') {
+        travelBase = travelPicker.value
+    }
+})
 
 yearPicker.value = currentDate.getFullYear()
 yearPicker.max = currentDate.getFullYear()
 yearPicker.min = currentDate.getFullYear() - 10
-
+monthPicker.value = currentDate.getMonth() + 1
 
 generateBtn.addEventListener('click', event => {
     event.preventDefault()
 
 
     let workbook = new Excel.Workbook();
-    let ws = workbook.addWorksheet('sheet1')
+    let ws = workbook.addWorksheet(`${monthPicker.value}.${yearPicker.value}`)
 
     let headerStyle = workbook.createStyle({
         font: {
@@ -43,11 +53,11 @@ generateBtn.addEventListener('click', event => {
 
 
     // write table headers
-    for (let col = 0; col < TITLES.length; col++) {
-        ws.cell(1, col + 1).string(TITLES[col]).style(headerStyle)
+    for (let col = 0; col < COLUMNS.length; col++) {
+        ws.cell(1, col + 1).string(COLUMNS[col]).style(headerStyle)
     }
 
-    let { numOfDays, firstDay } = monthInfo(currentDate.getMonth() + 1, currentDate.getFullYear())
+    let { numOfDays, firstDay } = monthInfo(monthPicker.value, yearPicker.value)
     for (let day = 1; day <= numOfDays; day++) {
         if (firstDay % 7 > 4) {
             ws.cell(day + 1, 1).string(DAYS_OF_WEEK[firstDay % 7]).style(weekendStyle) // days of week for weekend
@@ -58,25 +68,12 @@ generateBtn.addEventListener('click', event => {
         firstDay++
 
         // write extra hours formula
-
         ws.cell(day + 1, 4).formula(`IF(${FULL_HOURS}-C${day + 1}<0,C${day + 1}-${FULL_HOURS},0)`)
+
+        // write travels formula
+        ws.cell(day + 1, 5).formula(`${travelBase}*2*IF(C${day + 1}>0,1,0)`)
     }
 
-
-    // // Set value of cell A1 to 100 as a number type styled with paramaters of style
-    // ws.cell(1, 1).number(100).style(style);
-
-    // // Set value of cell B1 to 300 as a number type styled with paramaters of style
-    // ws.cell(1, 2).number(200).style(style);
-
-    // // Set value of cell C1 to a formula styled with paramaters of style
-    // ws.cell(1, 3).formula('A1 + B1').style(style);
-
-    // // Set value of cell A2 to 'string' styled with paramaters of style
-    // ws.cell(2, 1).string('string').style(style);
-
-    // // Set value of cell A3 to true as a boolean type styled with paramaters of style but with an adjustment to the font size.
-    // ws.cell(3, 1).bool(true).style(style).style({ font: { size: 14 } });
 
     workbook.write('Excel.xlsx');
 })
