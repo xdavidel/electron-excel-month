b906368756d6f1638bd03405492e064c5db3c98d
David Delarosa <xdavidel@gmail.com>
2020-04-20 14:02:18 +0300

    fix vulnerabilities & node usage in windows

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 
 let mainWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: `${__dirname}/assets/img/icon.png`
-    });
-    mainWindow.loadURL(`file://${__dirname}/pages/main-page/main-page.html`);
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		icon: `${__dirname}/assets/img/icon.png`,
+
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	mainWindow.loadURL(`file://${__dirname}/pages/main-page/main-page.html`);
 
 })
 
 app.on('window-all-closed', () => {
-    app.quit()
+	app.quit()
 })
